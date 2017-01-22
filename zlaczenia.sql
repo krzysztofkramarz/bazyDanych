deaf49cb11f7786fa7081b07bfb70d631dff1ccd
Krzysztof Kramarz <krzysztofkramarz@tlen.pl>
2017-01-22 19:16:54 +0100

    zlaczenia 1

--- zlaczenia.sql
+++ zlaczenia.sql
@@ -10,11 +10,41 @@ SELECT buyer_name, s.buyer_id, qty FROM buyers AS b INNER JOIN sales AS s  ON b.
 SELECT buyer_name, s.*, qty FROM buyers AS b , sales AS s  where buyer_name= 'Adam Barr'order by 1
 GO
 
+
 use Northwind
---5 Napisz polecenie zwracające nazwy produktów i firmy je dostarczające (baza northwind) 
+
+-- 1. Napisz polecenie zwracające nazwy produktów i firmy je dostarczające (baza northwind)
 -- tak aby produkty bez „dostarczycieli” i „dostarczyciele” bez produktów nie pojawiali się w wyniku.
-select companyname, productname from suppliers as s inner join products as p on s.supplierid = p.supplierid 
---6  Napisz polecenie zwracające jako wynik nazwy klientów, którzy złożyli zamówienia po 01 marca 1998 (baza northwind)
-select companyname, orderdate from orders inner join customers on orders.customerid = customers.customerid 
-where  orderdate > '3/1/98'
-go
+SELECT ProductName, CompanyName FROM Suppliers as s INNER JOIN Products as p ON s.SupplierId = p.SupplierId
+
+-- 2. Napisz polecenie zwracające jako wynik nazwy klientów, którzy złożyli zamówienia po 01 marca 1998 (baza northwind)
+SELECT CompanyName, OrderDate FROM Customers AS c INNER JOIN Orders AS o ON o.CustomerId = c.CustomerId 
+WHERE OrderDate > '3/1/98'
+-- lub ( (YEAR(Orders.OrderDate) = 1998 AND MONTH(Orders.OrderDate) > 2) OR YEAR(Orders.OrderDate) > 1998 )
+
+-- 3. Napisz polecenie zwracające wszystkich klientów z datami zamówień 
+SELECT CompanyName, OrderDate, c.CustomerId,  o.CustomerId  FROM Customers AS c INNER JOIN Orders AS o ON o.CustomerId = c.CustomerId 
+
+-- 4. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki (baza library). 
+-- Interesuje nas imię, nazwisko i data urodzenia dziecka. oraz moje: obecny wiek
+Use Library
+SELECT FirstName, Birth_Date , DATEDIFF(YY, Birth_Date, CURRENT_TIMESTAMP) FROM Member as m JOIN Juvenile as j  ON m.Member_no = j.Member_no
+
+-- 5. Napisz polecenie, które wyświetla listę dzieci będących członkami biblioteki. Interesuje nas imię, nazwisko,
+-- data urodzenia dziecka, adres zamieszkania dziecka oraz imię i nazwisko rodzica.
+select mj.firstname as FirstName, mj.lastname as LastName, birth_date, city, street, state,
+ma.firstname as AdultFirstName, ma.lastname as AdultLastName 
+from juvenile
+join member as mj on juvenile.member_no = mj.member_no
+join adult on adult.member_no = juvenile.adult_member_no
+join member as ma on ma.member_no = adult.member_no
+
+
+-- 6. Napisz polecenie, które podaje tytuły aktualnie wypożyczonych książek
+SELECT DISTINCT t.title FROM Title AS t 
+JOIN Loan AS l ON t.title_no = l.title_no
+
+
+
+
+Go
